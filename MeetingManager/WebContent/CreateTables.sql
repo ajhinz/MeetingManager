c086b2628220ded0b8d70cb1d4a8a480d8b30c96
Adam Hinz <adam.hinz@gmail.com>
2012-10-28 22:23:50 -0400

    Added three other tables to be created.

--- MeetingManager/WebContent/CreateTables.sql
+++ MeetingManager/WebContent/CreateTables.sql
@@ -3,3 +3,23 @@ id INTEGER PRIMARY KEY,
 name VARCHAR,
 team VARCHAR,
 build_id INTEGER);
+
+CREATE TABLE location (
+id INTEGER PRIMARY KEY,
+name VARCHAR,
+building_number INTEGER,
+city VARCHAR);
+
+CREATE TABLE meeting (
+id INTEGER PRIMARY KEY,
+location INTEGER REFERENCES location(id),
+start_time timestamp,
+end_type timestamp,
+minutes VARCHAR,
+created_by INTEGER REFERENCES employee(id));
+
+CREATE TABLE invited (
+id INTEGER PRIMARY KEY,
+meeting_id INTEGER REFERENCES meeting(id),
+employee_id INTEGER REFERENCES employee(id),
+response boolean);
