1ed021e67bc5a018ffae9f7e841ea417b2855f2d
Adam Hinz <adam.hinz@gmail.com>
2012-11-18 09:09:44 -0500

    Added meeting and invited sample data

--- MeetingManager/WebContent/SampleData.sql
+++ MeetingManager/WebContent/SampleData.sql
@@ -10,3 +10,17 @@ INSERT INTO location (id, name, building_number, city) VALUES (1, 'Tudor Room', 
 INSERT INTO location (id, name, building_number, city) VALUES (2, 'Sycamore Room', '254', 'Ashburn');
 INSERT INTO location (id, name, building_number, city) VALUES (3, 'Panther Room', '93', 'Fairfax');
 INSERT INTO location (id, name, building_number, city) VALUES (4, 'Halloween Room', '787', 'Sleepy Hallow');
+
+INSERT INTO meeting (id, location, start_time, end_time, created_by) VALUES (1, 1, '2012-11-15 14:30:00', '2012-11-15 15:30:00', 1);
+INSERT INTO meeting (id, location, start_time, end_time, created_by) VALUES (2, 2, '2012-11-17 13:00:00', '2012-11-17 15:00:00', 2);
+INSERT INTO meeting (id, location, start_time, end_time, created_by) VALUES (3, 3, '2012-11-18 12:30:00', '2012-11-18 15:30:00', 5);
+INSERT INTO meeting (id, location, start_time, end_time, created_by) VALUES (4, 4, '2012-11-19 10:00:00', '2012-11-19 11:30:00', 6);
+
+INSERT INTO invited (id, meeting_id, employee_id, response) VALUES (1, 1, 1, false);
+INSERT INTO invited (id, meeting_id, employee_id, response) VALUES (2, 1, 2, false);
+INSERT INTO invited (id, meeting_id, employee_id, response) VALUES (3, 2, 2, false);
+INSERT INTO invited (id, meeting_id, employee_id, response) VALUES (4, 2, 3, false);
+INSERT INTO invited (id, meeting_id, employee_id, response) VALUES (5, 3, 3, false);
+INSERT INTO invited (id, meeting_id, employee_id, response) VALUES (6, 3, 4, false);
+INSERT INTO invited (id, meeting_id, employee_id, response) VALUES (7, 4, 5, false);
+INSERT INTO invited (id, meeting_id, employee_id, response) VALUES (8, 4, 6, false);
