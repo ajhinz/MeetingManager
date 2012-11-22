b919d8f702f8ed89a24e6847fb06745fff04c527
Adam Hinz <adam.hinz@gmail.com>
2012-11-22 12:04:00 -0500

    Added forward and backward links to action for viewing schedule.

--- MeetingManager/WebContent/Today.jsp
+++ MeetingManager/WebContent/Today.jsp
@@ -1,11 +1,23 @@
 <%@ page contentType="text/html" pageEncoding="UTF-8"%>
 <%@ taglib prefix="t" tagdir="/WEB-INF/tags" %>
+<%@ taglib prefix="s" uri="/struts-tags" %>
 
 <t:template>
 	<jsp:attribute name="title">
-		Today's Schedule
+		<s:property value="day" /> Schedule
 	</jsp:attribute>
     <jsp:body>
-        <p>Today's schedule</p>
+        <p><s:property value="day" /> Schedule</p>
+		<p>
+        	<s:url action="today" var="yesterday">
+        		<s:param name="day"><s:date name="yesterday" format="yyyyMMdd" /></s:param>
+        	</s:url>
+        	<s:url action="today" var="tomorrow">
+        		<s:param name="day"><s:date name="tomorrow" format="yyyyMMdd" /></s:param>
+        	</s:url>
+			<a href="${yesterday}"><<</a>
+			<a href="${tomorrow}">>></a>
+		
+		</p>
     </jsp:body>
 </t:template>
--- MeetingManager/src/meetingmanager/TodayAction.java
+++ MeetingManager/src/meetingmanager/TodayAction.java
@@ -1,12 +1,68 @@
 package meetingmanager;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Map;
+
+import org.apache.struts2.interceptor.ParameterAware;
+import org.apache.struts2.interceptor.SessionAware;
+
+import util.Database;
+
 import com.opensymphony.xwork2.ActionSupport;
 
-public class TodayAction extends ActionSupport {
+public class TodayAction extends ActionSupport implements SessionAware {
 	private static final long serialVersionUID = 1L;
-
+	private Map<String, Object> session;
+	private Date day = new Date();
+	private Map params;
+	
 	public String execute() throws Exception {
+		Database db = new Database();
+		Map<String, Object> session = getSession();
+		Employee user = (Employee)session.get("user");
+		if (user == null) {
+			user = new Employee(1, db);
+		}
+		
 		return SUCCESS;
 	}
 
+	public void setSession(Map<String, Object> session) {
+		this.session = session;
+	}
+
+	private Map<String, Object> getSession() {
+		return this.session;
+	}
+	public void setDay(String day) {
+		SimpleDateFormat parser = new SimpleDateFormat("yyyyMMdd");
+		try {
+			this.day = parser.parse(day);
+		} catch (ParseException e) {
+			// Defaults to today
+		}
+	}
+	
+	public Date getDay() {
+		return this.day;
+	}
+	
+	public Date getYesterday() {
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(this.day);
+		calendar.add(Calendar.DATE, -1);
+		return calendar.getTime();
+	}
+	
+	public Date getTomorrow() {
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(this.day);
+		calendar.add(Calendar.DATE, 1);
+		return calendar.getTime();
+	}
+	
 }
