5ec22cb19b4a009b55e99fc567a8842fb525dc60
Adam Hinz <adam.hinz@gmail.com>
2012-11-28 22:33:52 -0500

    Drop sequences when dropping tables

--- MeetingManager/WebContent/DropTables.sql
+++ MeetingManager/WebContent/DropTables.sql
@@ -2,3 +2,7 @@ DROP TABLE invited;
 DROP TABLE meeting;
 DROP TABLE employee;
 DROP TABLE location;
+DROP SEQUENCE meeting_seq;
+DROP SEQUENCE invited_seq;
+DROP SEQUENCE employee_seq;
+DROP SEQUENCE location_seq;
