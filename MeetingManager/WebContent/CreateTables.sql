67a8e34d8350189fc3ec405db966825a2867de39
Adam Hinz <adam.hinz@gmail.com>
2012-12-06 15:17:46 -0500

    Start sequences at 100.  Send user to login page if not logged in.

--- MeetingManager/WebContent/CreateTables.sql
+++ MeetingManager/WebContent/CreateTables.sql
@@ -24,10 +24,10 @@ meeting_id INTEGER REFERENCES meeting(id),
 employee_id INTEGER REFERENCES employee(id),
 response boolean);
 
-CREATE SEQUENCE employee_seq;
+CREATE SEQUENCE employee_seq START 100;
 
-CREATE SEQUENCE location_seq;
+CREATE SEQUENCE location_seq START 100;
 
-CREATE SEQUENCE meeting_seq;
+CREATE SEQUENCE meeting_seq START 100;
 
-CREATE SEQUENCE invited_seq;
+CREATE SEQUENCE invited_seq START 100;
